2950750eacf75519d62c9ec189d75780a15e5e28
ftp5500 <ftp5500@yahoo.com>
2022-03-22 06:52:57 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,28 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<Color> color = [Colors.blue, Colors.red, Colors.green, Colors.purple];
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              for (var i in color)
+                Container(
+                  color: i,
+                  width: double.infinity,
+                  height: 150,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "${i}",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
