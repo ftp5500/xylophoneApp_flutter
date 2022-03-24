5eb35cec4aaf7bc91fa4870872ad10d1bac2ff17
ftp5500 <ftp5500@yahoo.com>
2022-03-24 06:41:17 +0300

    final project

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.amberAccent,
+    Colors.green,
+    Colors.lightBlueAccent,
+    Colors.blue,
+    Colors.deepPurple
+  ];
+
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7];
+
+  void playAudio(int audioNumber) {
+    final audioPlayer = AudioCache();
+    audioPlayer.play("note$audioNumber.wav");
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<Color> color = [Colors.blue, Colors.red, Colors.green, Colors.purple];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
-            children: [
-              for (var i in color)
-                Container(
-                  color: i,
-                  width: double.infinity,
-                  height: 150,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "${i}",
-                      style: TextStyle(color: Colors.black),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                for (var i = 0; i < colors.length; i++)
+                  Expanded(
+                    child: Container(
+                      color: colors[i],
+                      child: TextButton(
+                        onPressed: () {
+                          playAudio(i + 1);
+                        },
+                        child: Text(
+                          "${numbers[i]}",
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
